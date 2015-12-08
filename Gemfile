c7fe3dc1aab2e182d9954f3227a53b7a66ded530
Jesse Wolgamott <jesse@comalproductions.com>
2015-12-08 11:38:39 -0600

    wolf distance from earth to sun in hectares

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source 'http://rubygems.org'
 
 gem 'slack-ruby-bot'
+gem 'dotenv'
+gem 'wolfram'
+gem 'pry'
--- rubybot.rb
+++ rubybot.rb
@@ -1,4 +1,7 @@
 require 'slack-ruby-bot'
+Dotenv.load
+Wolfram.appid= ENV["WOLFRAM_APPID"]
+require 'uri'
 
 module RubyBot
   class App < SlackRubyBot::App
@@ -7,6 +10,36 @@ module RubyBot
   class Hiiii < SlackRubyBot::Commands::Base
     command 'hi' do |client, data, _match|
       client.message text: 'hiiiiiiiiiiii', channel: data.channel
+    end
+  end
+
+
+  class WolframSearch < SlackRubyBot::Commands::Base
+
+
+    command 'wolf' do |client, data, _match|
+
+      q = _match[:expression]
+      result = Wolfram::Query.new(q).fetch
+      hash = Wolfram::HashPresenter.new(result).to_hash
+
+      result = ""
+
+      hash.fetch(:pods, {}).each do |key, values|
+        next if values.join("") == ""
+        result << "\n" + key + "\n"
+        result << values.join("\n")
+      end
+
+      if result != ""
+        client.message text: result, channel: data.channel
+      else
+        client.message text: "
+          No results found for #{q}. \n
+          Try https://duckduckgo.com/?q=#{URI.escape(q)}+!google",
+          channel: data.channel
+      end
+
     end
   end
 end
